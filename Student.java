dd247acef82dd4ea83db0a525319755c5002faaa
FadingDarkness <snbutle@gmail.com>
2013-10-12 14:43:25 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,60 @@
-//This will be for the student class 
+//This is the student class, it creates the student object, which stores
+//which stores their name and gender
 
-public static void main(String [] args)
+
+public class Student
 {
-  System.out.println("this is a test");
+	private String name;
+	private boolean isMale;
+	private Student [] student;
+	private int arrCount=0;
+
+	public Student(String stuName, boolean gender)
+	{
+		name = stuName;
+		isMale = gender;
+	}
+
+	public boolean isMale()
+	{
+		return isMale;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getNumStu()
+	{
+		return arrCount +1;
+	}
+
+	public void addStu(String stuName, boolean gender)
+	{
+		Student newStu = new Student(stuName, gender);
+		student[arrCount++] = newStu;
+	}
+
+	public void remove(String stuName)
+	{
+		boolean stuFound = false;
+		int num = 0;
+		
+
+		while(stuFound = false)
+		{	
+			if(student[num++].getName().equals(stuName))
+			{
+				stuFound = true;
+				while(arrCount > num)
+				{
+					student[num++] = student[num+1];
+				}
+				arrCount --;
+			}	
+		}
+
+	}
+
 }
-
-
